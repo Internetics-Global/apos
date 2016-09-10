a752a7fe57363a470aaed25ae3bb62daea66c365
Clive France <clive@internetics.net.au>
2016-09-10 16:45:17 +1000

    1st commit

---
+++ app.js
@@ -0,0 +1,88 @@
+var apos = require('apostrophe')({
+  shortName: 'apos',
+  title: 'apos',
+
+  // These are the modules we want to bring into the project.
+  modules: {
+    // This configures the apostrophe-users module to add an admin-level
+    // group by default
+    'apostrophe-users': {
+      groups: [
+        {
+          title: 'guest',
+          permissions: [ ]
+        },
+        {
+          title: 'admin',
+          permissions: [ 'admin' ]
+        }
+      ]
+    },
+    // This configures the apostrophe-assets module to push a 'site.less'
+    // stylesheet by default
+    'apostrophe-assets': {
+      stylesheets: [
+        {
+          name: 'site'
+        }
+      ]
+    },
+    
+    
+    
+    // Search
+    'apostrophe-search': {
+    
+         filters: [
+        {
+          name: 'default',
+          label: 'Default'
+        },
+       
+      ]
+   
+    },
+    
+    
+    
+    
+    // Add your modules and their respective configuration here!
+
+
+ // This configures our default page template
+    'apostrophe-pages': {
+    
+    filters: {
+      
+        // Grab our ancestor pages, with two levels of subpages
+        ancestors: {
+          children: {
+            depth: 10
+          }
+        },
+        // We usually want children of the current page, too
+        children: true
+      },
+    
+
+      types: [
+        {
+          name: 'default',
+          label: 'Default'
+        },
+        {
+          name: 'home',
+          label: 'Home'
+        }
+      ]
+    },
+
+
+
+
+
+  }
+  
+
+
+});
